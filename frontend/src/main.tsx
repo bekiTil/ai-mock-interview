d7b08e1b2735c0c48cca1f0cb5824f2aab1594bc
bekiTil <btilahun287@gmail.com>
2026-04-24 14:40:22 -0400

    added web-analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next"
 
 import "./styles/tokens.css";   // <-- must come before App.css
 import "./App.css";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 );
